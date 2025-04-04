39d77bc7d3cc4fbaf7fd7c21484cf451e3daff95
Nicolas <nicolas.migues97@gmail.com>
2025-04-03 23:48:59 -0300

    ZombieUpgrade

--- Assets/Enemies/Scripts/ZombieBehaviour.cs
+++ Assets/Enemies/Scripts/ZombieBehaviour.cs
@@ -4,8 +4,10 @@ public class ZombieBehaviour : MonoBehaviour
 {
     [SerializeField]private NavMeshAgent _agent;
     [SerializeField]private ZombieAnimations _anims;
+    [SerializeField][Tooltip("Vida del zombie")]private int _life;
     private void Awake()
     {
+        _life = 50;
         _agent = GetComponent<NavMeshAgent>();
         _anims = GetComponent<ZombieAnimations>();
     }
@@ -18,14 +20,20 @@ public class ZombieBehaviour : MonoBehaviour
         if (Vector3.Distance(this.transform.position, GameManager.instance.player.transform.position) > 50)
         {
             _anims.ChangeState(STATE.Idle);
+            _agent.isStopped = true;
+            _agent.SetDestination(_agent.transform.position);
         }
         else if(Vector3.Distance(this.transform.position, GameManager.instance.player.transform.position) > 2)
         {
             _anims.ChangeState(STATE.Run);
+            _agent.isStopped = false;
+            _agent.SetDestination(GameManager.instance.player.transform.position);
         }
         else if(Vector3.Distance(this.transform.position, GameManager.instance.player.transform.position) < 2)
         {
             _anims.ChangeState(STATE.Atack);
+            _agent.isStopped = true;
+            _agent.SetDestination(_agent.transform.position);
         }
     }
 }
