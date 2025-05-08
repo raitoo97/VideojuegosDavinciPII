4dbff4015ad07f0dfaba0d2cc3db24a1c2b83f04
Nicolas <nicolas.migues97@gmail.com>
2025-05-08 00:38:17 -0300

    ExtendedClass

---
+++ Assets/ExtendedClassManager.cs
@@ -0,0 +1,20 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class ExtendedClassManager
+{
+    /// <summary>
+    /// Funcion que chequea la distancia entre dos objetos
+    /// </summary>
+    /// <param name="a">Transform desde el cual se llama el método (origen).</param>
+    /// <param name="b">Transform con el que se quiere comparar la distancia (destino).</param>
+    /// <param name="c">Distancia máxima permitida entre ambos objetos</param>
+    /// <returns>Devuelve <c>true</c> si la distancia entre ambos objetos es menor que <paramref name="c"/>; de lo contrario, <c>false</c>.</returns>
+    public static bool IsWithinDistanceOf(this Transform a, Transform b, float c)
+    {
+        var _distanciaAlCuadrado = (a.position - b.position).sqrMagnitude;
+        var _chequeDistancia = c * c;
+        return (_distanciaAlCuadrado < _chequeDistancia);
+    }
+}
