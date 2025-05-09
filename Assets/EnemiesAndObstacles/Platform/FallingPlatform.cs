9092cf07b3b114987e3a358ab951ea7ec83910b6
Nicolas <nicolas.migues97@gmail.com>
2025-05-09 00:30:18 -0300

    CleanCodeAndMap

--- Assets/EnemiesAndObstacles/Platform/FallingPlatform.cs
+++ Assets/EnemiesAndObstacles/Platform/FallingPlatform.cs
@@ -1,17 +1,11 @@
-using System;
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class FallingPlatform : MonoBehaviour
 {
     public float delayBeforeDissapear = 3f;
     public float delayBeforeReset = 1f;
-
     private bool hasTriggered = false;
     private Material material;
     private Color originalColor;
-
     private void Start()
     {
         Renderer rend = GetComponent<Renderer>();
@@ -20,9 +14,7 @@ public class FallingPlatform : MonoBehaviour
             material = rend.material;
             originalColor = material.color;
         }
-        
     }
-
     private void OnCollisionEnter(Collision other)
     {
         if (!hasTriggered && other.gameObject.CompareTag("Player"))
@@ -36,20 +28,17 @@ public class FallingPlatform : MonoBehaviour
             Invoke(nameof(StartDisappearing), delayBeforeDissapear);
         }
     }
-
     private void ChangeVisual()
     {
         Color color = material.color;
         color.a *= 0.5f; // reduce la opacidad
         material.color = color;
     }
-
     private void StartDisappearing()
     {
         gameObject.SetActive(false);
         Invoke(nameof(ResetPlatform), delayBeforeReset);
     }
-
     private void ResetPlatform()
     {
         gameObject.SetActive(true);
