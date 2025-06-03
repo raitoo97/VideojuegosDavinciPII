f40b30f966ae3cb65f50c32766f31003dd140d54
Nicolas <nicolas.migues97@gmail.com>
2025-06-03 02:28:39 -0300

    CleanCode

---
+++ Assets/Pj/Skills/Turret/RayCastTurretPj.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RayCastTurretPj
+{
+    private LayerMask _mask;
+    private Transform _transform;
+    private float _distance;
+    private bool _enabled;
+    public RayCastTurretPj(Transform transform, LayerMask mask, float distance)
+    {
+        _transform = transform;
+        _mask = mask;
+        _distance = distance;
+    }
+    public void OnUpdate()
+    {
+        Ray _ray = new Ray(_transform.position, _transform.forward);
+        if (Physics.Raycast(_ray, out RaycastHit _hit, _distance, _mask))
+        {
+            if (_hit.transform.gameObject.TryGetComponent<Player>(out var player))
+            {
+                _enabled = true;
+            }
+            else
+            {
+                _enabled = false;
+            }
+        }
+        else
+        {
+            _enabled = false;
+        }
+        Debug.DrawRay(_ray.origin, _ray.direction * _distance, Color.red);
+    }
+    public bool IsEnabled { get => _enabled; }
+}
