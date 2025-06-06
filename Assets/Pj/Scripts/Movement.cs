ed5f603a46c86ed3d9bc810b788fbd16247ca3da
Ignacio Garcés <ignaciogarces96@gmail.com>
2025-06-06 14:27:18 -0300

    Dash Sound

--- Assets/Pj/Scripts/Movement.cs
+++ Assets/Pj/Scripts/Movement.cs
@@ -45,8 +45,8 @@ public class Movement
         if (_rb.velocity.magnitude != 0)
         {
             _rb.AddForce(dashDirection.normalized * impulse, ForceMode.Impulse);
-            int randomIndex = UnityEngine.Random.Range(0, AudioManager.instance.turretPlayerImpactSfx.Length);
-            AudioManager.instance.PlaySfxRandomPitch(AudioManager.instance.turretPlayerImpactSfx[randomIndex]); //sound effect
+            int randomIndex = UnityEngine.Random.Range(0, AudioManager.instance.skillPlayerDash.Length);
+            AudioManager.instance.PlaySfxRandomPitch(AudioManager.instance.skillPlayerDash[randomIndex]); //sound effect
             ParticlesPool.instance.SpamParticle(ParticleType.Dash, new Vector3(0f, 0f, 1f), Vector3.zero, _rb.transform);
         }
     }
