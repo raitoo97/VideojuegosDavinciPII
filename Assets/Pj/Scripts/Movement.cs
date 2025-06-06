51113d150e6b3b14672f658851e7deae0710d0b8
Ignacio Garcés <ignaciogarces96@gmail.com>
2025-06-06 02:00:08 -0300

    +

--- Assets/Pj/Scripts/Movement.cs
+++ Assets/Pj/Scripts/Movement.cs
@@ -47,7 +47,7 @@ public class Movement
             _rb.AddForce(dashDirection.normalized * impulse, ForceMode.Impulse);
             int randomIndex = UnityEngine.Random.Range(0, AudioManager.instance.turretPlayerImpactSfx.Length);
             AudioManager.instance.PlaySfxRandomPitch(AudioManager.instance.turretPlayerImpactSfx[randomIndex]); //sound effect
-            ParticlesPool.instance.SpamParticle(ParticleType.Dash, new Vector3(0f, 2f, 0f), Vector3.zero, _rb.transform);
+            ParticlesPool.instance.SpamParticle(ParticleType.Dash, new Vector3(0f, 0f, 0f), Vector3.zero, _rb.transform);
         }
     }
     public void StopDash()
