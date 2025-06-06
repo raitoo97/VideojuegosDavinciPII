229cce2194dde9cdc06b71b176ee6ee19df1bd69
Ignacio Garcés <ignaciogarces96@gmail.com>
2025-06-05 23:17:52 -0300

    Working

--- Assets/Pj/Scripts/ControlPlayer.cs
+++ Assets/Pj/Scripts/ControlPlayer.cs
@@ -1,11 +1,13 @@
-using System;
 using UnityEngine;
+using System;
 public class ControlPlayer
 {
     Movement _movement;
     PlayerAnimation _animation;
     bool _wasHoldingShift = false;
     bool _wasInGround;
+    public float horizontal;
+    public float vertical;
 
     // Dash
     bool canDash = true;
@@ -16,22 +18,17 @@ public class ControlPlayer
     bool isDashing = false;
     float dashImpulse = 20f;
 
-    // Input guardado
-    float horizontal;
-    float vertical;
-    float dodgeSpeedMultiplier = 1f;
-
     public ControlPlayer(Movement m, PlayerAnimation a)
     {
         _movement = m;
         _animation = a;
     }
+    public void OnUpdate()
 
-    public void HandleInput() // Se llama en Update
     {
+        //Input
         horizontal = Input.GetAxis("Horizontal");
         vertical = Input.GetAxis("Vertical");
-
         var foward = MathF.Abs(horizontal) + MathF.Abs(vertical);
         foward = Math.Clamp(foward, 0f, 1f);
         bool isFoward = foward != 0;
@@ -40,7 +37,96 @@ public class ControlPlayer
         bool isJumping = Input.GetKeyDown(KeyCode.Space);
         bool isDash = Input.GetKeyDown(KeyCode.Mouse0); // botón de dash
 
-        dodgeSpeedMultiplier = 1f;
+        // -------ANIMACIONES DE MOVIMIENTO------
+        _animation.SetGround("ground", isGrounded);
+        // Cae (solo cuando estaba en el suelo y ahora no lo está)
+        if (!isGrounded && _wasInGround)
+        {
+            _animation.SetJump("jump", true);
+            _animation.SetIdle("idle", false);
+            _animation.SetWalk("walk", 0f);
+        }
+        // Está en el aire (sigue saltando) Se mantiene la animación de salto
+        else if (!isGrounded)
+        {
+            _animation.SetJump("jump", true);
+            _animation.SetIdle("idle", false);
+            _animation.SetWalk("walk", 0f);
+        }
+        // Está en el suelo
+        else
+        {
+            _animation.SetJump("jump", false);
+            if (isFoward)
+            {
+                _animation.SetIdle("idle", false);
+                _animation.SetWalk("walk", foward);
+            }
+            else
+            {
+                _animation.SetWalk("walk", 0f);
+                _animation.SetIdle("idle", true);
+            }
+        }
+        //          ======== JUMP ========
+        if (isJumping && isGrounded && !isDodgeMode)
+        {
+            float jumpForce = 8f;
+            _animation.SetJump("jump", true);
+            _movement.Jump(jumpForce);
+        }
+        //          ======== DODGE Y TRANSFORMING ========
+        if (isDodgeMode && !_wasHoldingShift)
+        {
+            _animation.SetTransforming("transforming", true); // Modo Bola
+            var turretPj = GameObject.FindAnyObjectByType<TurretPj>();
+            if (turretPj != null)
+            {
+                turretPj.DesactivateSelf();
+                GameManager.instance.player.GetComponent<Player>().GetMovement.ChangeSpeed(20f);//Acelero la velocidad del player
+            }
+        }
+        if (!isDodgeMode && _wasHoldingShift)
+        {
+            _animation.SetTransforming("transforming", false); // Volvió al modo trípode
+            var turretPj = GameObject.FindAnyObjectByType<TurretPj>();
+            if (turretPj != null)
+            {
+                turretPj.ActivateSelf();
+                GameManager.instance.player.GetComponent<Player>().GetMovement.ChangeSpeed(GameManager.instance.player.GetComponent<Player>().GetInitSpeed);//Toma la velocidad normal del player
+            }
+        }
+        if (isDodgeMode && isFoward && isGrounded)
+        {
+            _animation.SetTransforming("transforming", true);
+            _animation.SetDodge("dodging", foward);
+            _animation.SetIdle("idle", false);
+        }
+        else if (isDodgeMode && !isFoward && isGrounded)
+        {
+            _animation.SetDodge("dodging", 0f);
+            _animation.SetIdle("idle", true);
+        }
+        else
+        {
+            _animation.SetDodge("dodging", 0f);
+        }
+        if (isGrounded && !isDodgeMode)
+        {
+            var turretPj = GameObject.FindAnyObjectByType<TurretPj>();
+            if (turretPj != null)
+            {
+                turretPj.ActivateSelf();
+            }
+        }
+        else
+        {
+            var turretPj = GameObject.FindAnyObjectByType<TurretPj>();
+            if (turretPj != null)
+            {
+                turretPj.DesactivateSelf();
+            }
+        }
 
         // ---------- DASH ----------
         if (isDash && canDash && !isDodgeMode && isGrounded)
@@ -59,7 +145,7 @@ public class ControlPlayer
             if (dashTimer <= 0f)
             {
                 isDashing = false;
-                _movement.StopDash(); // Este método frena el dash
+                _movement.StopDash(); // Este mtodo frena el dash
                 //_animation.SetDash("dash", false); animacion si quieren poner
             }
         }
@@ -72,95 +158,13 @@ public class ControlPlayer
                 canDash = true;
             }
         }
-
-        // ---------- ANIMACIONES ----------
-        _animation.SetGround("ground", isGrounded);
-
-        if (!isGrounded && _wasInGround)
-        {
-            _animation.SetJump("jump", true);
-            _animation.SetIdle("idle", false);
-            _animation.SetWalk("walk", 0f);
-        }
-        else if (!isGrounded)
-        {
-            _animation.SetJump("jump", true);
-            _animation.SetIdle("idle", false);
-            _animation.SetWalk("walk", 0f);
-        }
-        else
-        {
-            _animation.SetJump("jump", false);
-            if (isFoward)
-            {
-                _animation.SetIdle("idle", false);
-                _animation.SetWalk("walk", foward);
-            }
-            else
-            {
-                _animation.SetWalk("walk", 0f);
-                _animation.SetIdle("idle", true);
-            }
-        }
-
-        // ---------- JUMP ----------
-        if (isJumping && isGrounded && !isDodgeMode)
-        {
-            float jumpForce = 8f;
-            _animation.SetJump("jump", true);
-            _movement.Jump(jumpForce);
-        }
-
-        // ---------- TRANSFORMACIÓN ----------
-        if (isDodgeMode && !_wasHoldingShift)
-        {
-            _animation.SetTransforming("transforming", true);
-            var turretPj = GameObject.FindAnyObjectByType<TurretPj>();
-            turretPj?.DesactivateSelf();
-            GameManager.instance.player.GetComponent<Player>().GetMovement.ChangeSpeed(8f);
-        }
-
-        if (!isDodgeMode && _wasHoldingShift)
-        {
-            _animation.SetTransforming("transforming", false);
-            var turretPj = GameObject.FindAnyObjectByType<TurretPj>();
-            turretPj?.ActivateSelf();
-            GameManager.instance.player.GetComponent<Player>().GetMovement.ChangeSpeed(
-                GameManager.instance.player.GetComponent<Player>().GetInitSpeed
-            );
-        }
-
-        // ---------- DODGE MODE ----------
-        if (isDodgeMode && isFoward && isGrounded)
-        {
-            _animation.SetTransforming("transforming", true);
-            _animation.SetDodge("dodging", foward);
-            _animation.SetIdle("idle", false);
-            dodgeSpeedMultiplier = 2f;
-        }
-        else if (isDodgeMode && !isFoward && isGrounded)
-        {
-            _animation.SetDodge("dodging", 0f);
-            _animation.SetIdle("idle", true);
-            dodgeSpeedMultiplier = 1f;
-        }
-        else
-        {
-            _animation.SetDodge("dodging", 0f);
-        }
-
-        // ---------- TORRETA ----------
-        var turretCheck = GameObject.FindAnyObjectByType<TurretPj>();
-        if (isGrounded && !isDodgeMode) turretCheck?.ActivateSelf();
-        else turretCheck?.DesactivateSelf();
-
+        //      ======== MOVIMIENTO FÍSICO Y ESTADO ========
         _wasHoldingShift = isDodgeMode;
         _wasInGround = isGrounded;
-    }
-
-    public void OnFixedUpdate() // Se llama en FixedUpdate
-    {
-        _movement.Move(horizontal, vertical, dodgeSpeedMultiplier);
         _movement.UpdateGroundCheck();
+    }
+    public void OnfixedUpdate()
+    {
+        _movement.Move(horizontal, vertical);
     }
 }
