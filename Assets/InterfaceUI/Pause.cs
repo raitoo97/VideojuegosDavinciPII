df9af56149b70f15301dc759d3ca24bb422a21e4
Nicolas <nicolas.migues97@gmail.com>
2025-06-27 13:54:50 -0300

    MenuInit

---
+++ Assets/InterfaceUI/Pause.cs
@@ -0,0 +1,59 @@
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+public class Pause : MonoBehaviour
+{
+    private bool _onPause;
+    [SerializeField]private GameObject _pauseMenu;
+    [SerializeField]private Button[] Buttons;
+    private void Awake()
+    {
+        _onPause = false;
+    }
+    void Start()
+    {
+        Buttons[0].onClick.AddListener(GoToMainMenu);
+        Buttons[1].onClick.AddListener(ExitGame);
+        Buttons[2].onClick.AddListener(ContinueButton);
+        _pauseMenu.SetActive(false);
+    }
+    private void MenuState()
+    {
+        if (!_onPause)
+        {
+            PauseButton();
+        }
+        else
+        {
+            ContinueButton();
+        }
+    }
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            MenuState();
+        }
+    }
+    private void GoToMainMenu()
+    {
+        SceneManager.LoadScene(0);
+    }
+    private void ExitGame()
+    {
+        Application.Quit();
+        Debug.Log("Funciona solo en build");
+    }
+    private void ContinueButton()
+    {
+        Time.timeScale = 1.0f;
+        _pauseMenu.SetActive(false);
+        _onPause = false;
+    }
+    private void PauseButton()
+    {
+        Time.timeScale = 0.0f;
+        _pauseMenu.SetActive(true);
+        _onPause = true;
+    }
+}
