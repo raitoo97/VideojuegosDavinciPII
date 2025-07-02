c47e12c83c06620f25c6f79f5c7602a5021deb14
Nicolas <nicolas.migues97@gmail.com>
2025-07-02 17:24:31 -0300

    IntiUIUpdate

--- Assets/InterfaceUI/ObstaclesDetected.cs
+++ Assets/InterfaceUI/ObstaclesDetected.cs
@@ -1,7 +1,4 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class ObstaclesDetected : MonoBehaviour
 {
     // Start is called before the first frame update
