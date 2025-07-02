36150f809cd0b57a37378adf639921d3508e9113
Nicolas <nicolas.migues97@gmail.com>
2025-07-02 03:01:02 -0300

    Update

--- Assets/InterfaceUI/WavesUI.cs
+++ Assets/InterfaceUI/WavesUI.cs
@@ -45,6 +45,7 @@ public class WavesUI
         WavesManager.instance._currentWave?.Invoke();
         WavesManager.instance.AdvanceWave();
         WavesManager.instance.StartCoroutine(WavesManager.instance.GetWaveUIButton());
+        AudioManager.instance.PlayMusic(AudioManager.instance.buttonClick);
         _isFirstWave = false;
         _arrow.gameObject.SetActive(false);
     }
