ce928498550b9f78501c2ff688ca0701aaef61a1
Ignacio Garcés <ignaciogarces96@gmail.com>
2025-07-04 13:37:07 -0300

    Ready!

--- Assets/InterfaceUI/WavesUI.cs
+++ Assets/InterfaceUI/WavesUI.cs
@@ -8,7 +8,7 @@ public class WavesUI
     public Text _numberOfEnemies;
     public Text _winningText;
     public Text _arrow;
-    private bool _isFirstWave = false; //CAMBIO POARA LABUIRRAR
+    private bool _isFirstWave = true; //CAMBIO POARA LABUIRRAR
     public bool _isLastWave = false;
     private enum WaveAtributes
     {
