3064e3c2533e32b5587cae7fe480c6fe8407b678
Nicolas <nicolas.migues97@gmail.com>
2025-07-07 22:35:16 -0300

    IntiDoppleganger

--- Assets/Pj/Skills/ChestRandomSkill.cs
+++ Assets/Pj/Skills/ChestRandomSkill.cs
@@ -1,16 +1,14 @@
-using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
-
 public class ChestRandomSkill : MonoBehaviour
 {
+
     void Update()
     {
         if (Input.GetKeyDown(KeyCode.F))
         {
-            ManagerSkills.instance.UnlockSkillCategory(SkillCategory.dashCategory);
+            ManagerSkills.instance.UnlockSkillCategory(SkillCategory.dopplegangerCategory);
         }
-        print(ManagerSkills.instance.GetValueSkill(SkillCategory.dashCategory,SkillStatType.dashCooldown));
-        print(ManagerSkills.instance.GetValueSkill(SkillCategory.dashCategory, SkillStatType.dashSpeed));
+        print(ManagerSkills.instance.GetValueSkill(SkillCategory.dopplegangerCategory, SkillStatType.dopplegangerLife));
+        print(ManagerSkills.instance.GetValueSkill(SkillCategory.dopplegangerCategory, SkillStatType.coldowndoppleganger));
     }
 }
