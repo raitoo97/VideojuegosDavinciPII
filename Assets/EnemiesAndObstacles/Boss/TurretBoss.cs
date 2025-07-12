8f5a25414a3103d879a7ff120888c3a57851df0e
Nicolas <nicolas.migues97@gmail.com>
2025-07-12 12:58:14 -0300

    PreFix

--- Assets/EnemiesAndObstacles/Boss/TurretBoss.cs
+++ Assets/EnemiesAndObstacles/Boss/TurretBoss.cs
@@ -1,13 +1,13 @@
 using UnityEngine;
 public class TurretBoss : MonoBehaviour
 {
-    [SerializeField]private Vector3 _dirRotVector;
-    [SerializeField]private Quaternion _dirRotQuaternion;
     [SerializeField]private Transform _child;
     [SerializeField]private Transform _rayLaser;
     [SerializeField]private RayCastTurret _rayTurret;
     [SerializeField]private float _distance;
     [SerializeField]private Transform _gunSight;
+    private Vector3 _dirRotVector;
+    private Quaternion _dirRotQuaternion;
     public Material lineRendererMaterial;
     public LayerMask mask;
     private float _shootCooldown;
@@ -31,7 +31,7 @@ public class TurretBoss : MonoBehaviour
         _dirRotQuaternion = Quaternion.LookRotation(_dirRotVector);
         float tripodSpeed = GameManager.instance.player.GetComponent<Player>().GetInitSpeed * 2.5f;
         _child.transform.rotation = Quaternion.Slerp(_child.transform.rotation, _dirRotQuaternion, tripodSpeed * Time.deltaTime);
-        _rayTurret.OnUpdate();
+        //_rayTurret.OnUpdate();
         _shootCooldown -= Time.deltaTime;
         if (_rayTurret.IsEnabled && _shootCooldown <= 0f)
         {
