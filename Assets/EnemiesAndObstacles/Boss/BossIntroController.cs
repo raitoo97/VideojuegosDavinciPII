a0658713eb29350faa79c2b90d636f300db566cd
Nicolas <nicolas.migues97@gmail.com>
2025-07-16 01:37:47 -0300

    PreCameras

--- Assets/EnemiesAndObstacles/Boss/BossIntroController.cs
+++ Assets/EnemiesAndObstacles/Boss/BossIntroController.cs
@@ -1,6 +1,8 @@
+using System.Collections;
 using UnityEngine;
 using UnityEngine.AI;
 using UnityEngine.Playables;
+using UnityEngine.UI;
 public class BossIntroController : MonoBehaviour
 {
     [Header("PLAYER")]
@@ -13,19 +15,31 @@ public class BossIntroController : MonoBehaviour
     [SerializeField] private Animator _bossAnimator;             
     [Header("TIMELINE")]
     [SerializeField] private PlayableDirector _bossIntroTimeline;
+    [Header("UI / FADE")]
+    [SerializeField]private Image _fadeImage;
+    [SerializeField]private Color _fadeColorinit;
+    [SerializeField]private Color _fadeColorfinish;
     private bool _hasPlayed = false;
-    public void StartIntroCinematic()
-    {
-        if (_hasPlayed) return;
-        _hasPlayed = true;
-        PlayCinematicSequence();
-    }
     private void Update()
     {
         if (Input.GetKeyDown(KeyCode.J))
         {
             _bossIntroTimeline.Play();
         }
+    }
+    public void StartIntroCinematic()
+    {
+        if (_hasPlayed) return;
+        _hasPlayed = true;
+        PlayCinematicSequence();
+    }
+    public void ActivateFadeOut()
+    {
+        StartCoroutine(FadeOut());
+    }
+    public void ActivateFadeIn()
+    {
+        StartCoroutine(FadeIn());
     }
     private void PlayCinematicSequence()
     {
@@ -36,6 +50,10 @@ public class BossIntroController : MonoBehaviour
         _bossAnimator.applyRootMotion = true;
         _bossAnimator.SetBool("IsCinematic", true);
     }
+    public void StartScreaming()
+    {
+        _bossAnimator.SetTrigger("StopWalk");
+    }
     public void EndIntroCinematic()
     {
         _playerController.enabled = true;
@@ -44,5 +62,31 @@ public class BossIntroController : MonoBehaviour
         _bossNavMesh.enabled = true;
         _bossAnimator.applyRootMotion = false;
         _bossAnimator.SetBool("IsCinematic", false);
+    }
+    private IEnumerator FadeOut()
+    {
+        float time = 0f;
+        float finishied_time = 2f;
+        while (time <= finishied_time)
+        {
+            _fadeImage.color = Color.Lerp(_fadeColorinit, _fadeColorfinish, time / finishied_time);
+            yield return new WaitForSeconds(0.2f);
+            time += 0.2f;
+        }
+        _fadeImage.color = _fadeColorfinish;
+        _fadeImage.gameObject.SetActive(false);
+    }
+    private IEnumerator FadeIn()
+    {
+        float time = 0f;
+        float finishied_time = 2f;
+        while (time <= finishied_time)
+        {
+            _fadeImage.color = Color.Lerp(_fadeColorfinish, _fadeColorinit, time / finishied_time);
+            yield return new WaitForSeconds(0.2f);
+            time += 0.2f;
+        }
+        _fadeImage.color = _fadeColorinit;
+        _fadeImage.gameObject.SetActive(true);
     }
 }
