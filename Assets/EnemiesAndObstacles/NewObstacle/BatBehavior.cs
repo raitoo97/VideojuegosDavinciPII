ec652bf843ad7b396513b543cba64cb88af18db4
Ignacio Garcés <ignaciogarces96@gmail.com>
2025-07-16 20:25:08 -0300

    Pool of bats working!

--- Assets/EnemiesAndObstacles/NewObstacle/BatBehavior.cs
+++ Assets/EnemiesAndObstacles/NewObstacle/BatBehavior.cs
@@ -8,27 +8,37 @@ public class BatBehavior : MonoBehaviour
     private float _speed = 10f;
     private float _damage = 30f;
     private float _existenceTime = 25f;
-
+    private Coroutine _despawnCoroutine;
     void FixedUpdate()
     {
         transform.position += transform.forward * _speed * Time.fixedDeltaTime;
     }
     private void OnEnable()
     {
-        StartCoroutine(Despawn());
+        if (_despawnCoroutine != null )
+        {
+            StopCoroutine(_despawnCoroutine);
+        }
+        _despawnCoroutine = StartCoroutine(Despawn());
     }
-
+    private void OnDisable()
+    {
+        if (_despawnCoroutine != null)
+        {
+            StopCoroutine(_despawnCoroutine);
+            _despawnCoroutine = null;
+        }
+    }
+    public IEnumerator Despawn()
+    {
+        yield return new WaitForSeconds(_existenceTime);
+        this.gameObject.SetActive(false);
+    }
     private void OnTriggerEnter(Collider other)
     {
         if (other.TryGetComponent<Player>(out Player player))
         {
             player.DamagePlayer(_damage);
         }
-    }
-
-    public IEnumerator Despawn()
-    {
-        yield return new WaitForSeconds(_existenceTime);
-        this.gameObject.SetActive(false);
     }
 }
--- Assets/EnemiesAndObstacles/NewObstacle/BatSpawner.cs
+++ Assets/EnemiesAndObstacles/NewObstacle/BatSpawner.cs
@@ -8,7 +8,8 @@ public class BatSpawner : MonoBehaviour
 {
     [SerializeField] GameObject _prefab;
     [SerializeField] private List <Transform> _spawnPoints = new List<Transform>();
-
+    [SerializeField] private List <GameObject> _bats= new List<GameObject>();
+    private int amount = 20;
     public bool canSpawnBats = true;
 
     private void Start()
@@ -17,22 +18,60 @@ public class BatSpawner : MonoBehaviour
         {
             _spawnPoints.Add(spawn);
         }
+
+
+        CompleteList(amount);
     }
 
     private void Update()
     {
         if (WavesManager.instance.waveStarted && canSpawnBats)
         {
-            StartCoroutine(Spawn(_prefab));
+            StartCoroutine(Spawn());
         }
     }
 
-    public IEnumerator Spawn(GameObject prefab)
+    public void CompleteList(int number)
+    {
+        for (int i = 0; i < number; i++)
+        {
+            var _clonedBat = GameObject.Instantiate(_prefab);
+            _clonedBat.SetActive(false);
+            
+            _bats.Add(_clonedBat);
+        }
+    }
+    public IEnumerator Spawn()
     {
         canSpawnBats = false;
-        Transform chosenSpawn = _spawnPoints[UnityEngine.Random.Range(0, _spawnPoints.Count)];
-        Instantiate(prefab, chosenSpawn.position, Quaternion.LookRotation(chosenSpawn.forward));
+        GetBat();
+        //Transform chosenSpawn = _spawnPoints[UnityEngine.Random.Range(0, _spawnPoints.Count)];
+        //Instantiate(prefab, chosenSpawn.position, Quaternion.LookRotation(chosenSpawn.forward));
         yield return new WaitForSeconds(5);
         canSpawnBats = true;
+    }
+
+    public GameObject GetBat()
+    {
+        Transform chosenSpawn = _spawnPoints[UnityEngine.Random.Range(0, _spawnPoints.Count)];
+
+        for (int i = 0; i < _bats.Count; i++)
+        {
+            if (!_bats[i].activeSelf)
+            {
+                _bats[i].SetActive(true);
+                _bats[i].transform.position = chosenSpawn.position;
+                _bats[i].transform.forward = chosenSpawn.forward;
+                return _bats[i];
+            }
+        }
+
+        CompleteList(1);
+        GameObject _auxBat = _bats[_bats.Count - 1];
+        _auxBat.transform.position = chosenSpawn.position;
+        _auxBat.transform.forward = chosenSpawn.forward;
+
+        _auxBat.SetActive(true);
+        return _auxBat;
     }
 }
